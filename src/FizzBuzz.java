fef347e894a2651b68494430304dbd4bcc7e69b5
zelaynia <allie.huynh@mail.utoronto.ca>
2024-09-09 10:15:08 -0400

    refactored to extract helper method

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -9,11 +9,11 @@ class FizzBuzz {
         while (i<100) {
             i ++;
             // Find out which numbers divide i.
-            extracted(i);
+            dofizzbuzz(i);\
         }
     }
-
-    private static void extracted(int i) {
+// to call this class FizzBuzz from outside file, you type FizzBuzz.dofizzbuzz
+    private static void dofizzbuzz(int i) {
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
 
